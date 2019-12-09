e7a41feeae9d8458d71bb76209b218ce85c9a587
lipeng1 <shein1@shein.com>
2019-12-09 22:51:24 +0800

    upload maven center

--- src/main/java/com/github/jcsv/importj/Validator.java
+++ src/main/java/com/github/jcsv/importj/Validator.java
@@ -7,6 +7,7 @@ import com.github.jcsv.importj.ColValidcateProperties;
  * @Date: 2019/9/1 00:24
  * @Description:
  */
+@FunctionalInterface
 public interface Validator {
     public boolean validcate(String value, ColValidcateProperties col, String[] row, Object params);
 }
